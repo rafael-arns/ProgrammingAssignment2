995fe837be103414cd40e43b9609424d25fa95ae
rafael-arns <rafarns.psi@gmail.com>
2015-05-24 16:24:23 -0300

    versao1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Adapted from the mean function in assignment 2
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set<- set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get<-function() x
+  setinverse <- function(solve) m<<- solve
+  getinverse <- function() m
+  list(set=set,get=get,setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Adapted from the mean function in assignment 2
+## return inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <-x$getinverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <-x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m    
+  ## Return a matrix that is the inverse of 'x'
 }
